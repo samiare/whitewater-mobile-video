3c064b96db996d08d6f7f39e1b652a9c63074a6e
Samir Zahran <sayhello@samiare.net>
2016-07-08 14:29:47 -0400

    Removed examples from master branch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,31 +27,13 @@ module.exports = function(grunt) {
         },
 
         clean: {
-            build: ['dist/*'],
-            examples: ['examples/examples.zip', 'examples/dist/*', '!examples/source/**']
+            build: ['dist/*']
         },
 
         jshint: {
-            files: ['gruntfile.js', 'source/*.js', 'examples/*.js'],
+            files: ['gruntfile.js', 'source/*.js'],
             options: {
                 validthis: true
-            }
-        },
-
-        copy: {
-            build: {
-                expand: true,
-                cwd: 'dist/',
-                src: '*',
-                dest: 'examples/js'
-            },
-            examples: {
-                files: [
-                    {expand: true, flatten: true, cwd: 'examples/source/', src: '*', dest: 'examples/dist/', filter: 'isFile'}, // index.html
-                    {expand: true, flatten: true, src: 'examples/source/accordion-content/*', dest: 'examples/dist/subpages'}, // sub-pages
-                    {expand: true, cwd: 'dist/', src: ['*.js', '*.map'], dest: 'examples/dist/js/'}, // .js and .js.map
-                    {expand: true, cwd: 'examples/source/', src: ['images/**', 'videos/**', 'fonts/**'], dest: 'examples/dist/'} // images, videos, fonts
-                ]
             }
         },
 
@@ -66,44 +48,19 @@ module.exports = function(grunt) {
                     expand: true,
                     src: '*'
                 }]
-            },
-
-            examples: {
-                options: {
-                    archive: 'examples/examples.zip',
-                    mode: 'zip'
-                },
-                files: [{
-                    cwd: 'examples/dist/',
-                    expand: true,
-                    src: '**'
-                }]
             }
-        },
-
-        compass: {
-            examples: {
-                options: {
-                    sassDir: 'examples/source/styles',
-                    cssDir: 'examples/dist/styles'
-                }
-            }
-        },
+        }
 
     });
 
 
-    grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-include-replace');
-    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.registerTask('hint', ['jshint']);
     grunt.registerTask('build', ['clean:build', 'includereplace:build', 'uglify', 'compress:build']);
-    grunt.registerTask('build-site', ['clean:examples', 'copy:examples', 'compass:examples', 'compress:examples']);
 
 };
